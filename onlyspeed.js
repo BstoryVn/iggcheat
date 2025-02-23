30d91bf4eab312af079530a43a36195da239c618
BstoryVn <110188679+BstoryVn@users.noreply.github.com>
2025-02-23 19:31:21 +0700

    Add files via upload

---
+++ onlyspeed.js
@@ -0,0 +1,19 @@
+function m100() {
+    h5gg.clearResults();
+    // Thử mã tốc độ mới
+    h5gg.searchNumber('4397530849764387586', 'I64', '0x100000000', '0x200000000'); // Mã gốc để tìm speed
+
+
+}
+
+function m101(input)
+{
+ var m101 = document.getElementById('m101');
+    if (m101.checked) {
+    h5gg.editAll('4366458311853765201', 'I64');
+}
+else {
+    h5gg.editAll('4397530849764387586', 'I64');
+
+}
+}
