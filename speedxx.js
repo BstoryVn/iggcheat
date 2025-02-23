0c7fc08bb56db13b6cc8431daf9beb26c9c36f34
BstoryVn <110188679+BstoryVn@users.noreply.github.com>
2025-02-23 19:28:09 +0700

    Update speedxx.js

--- speedxx.js
+++ speedxx.js
@@ -10,7 +10,7 @@ function m100() {
     h5gg.clearResults();
     // Thử mã tốc độ mới
     h5gg.searchNumber('4397530849764387586', 'I64', '0x100000000', '0x200000000'); // Mã gốc để tìm speed
-    alert('Done Find Speed_(' + h5gg.getResultsCount() + '/1)');
+
 
 }
 
@@ -32,25 +32,4 @@ else {
 
 }
 }
-document.addEventListener("DOMContentLoaded", function () {
-    var dragItem = document.getElementById("ayimgui");
-    var offsetX, offsetY, isDragging = false;
 
-    dragItem.addEventListener("mousedown", function (e) {
-        isDragging = true;
-        offsetX = e.clientX - dragItem.offsetLeft;
-        offsetY = e.clientY - dragItem.offsetTop;
-        dragItem.style.cursor = "grabbing"; // Change cursor when dragging
-    });
-
-    document.addEventListener("mousemove", function (e) {
-        if (!isDragging) return;
-        dragItem.style.left = e.clientX - offsetX + "px";
-        dragItem.style.top = e.clientY - offsetY + "px";
-    });
-
-    document.addEventListener("mouseup", function () {
-        isDragging = false;
-        dragItem.style.cursor = "grab"; // Reset cursor
-    });
-});
