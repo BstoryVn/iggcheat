85aec334a2e9126f70fbedd2dc5b7403b987a9fd
BstoryVn <110188679+BstoryVn@users.noreply.github.com>
2025-03-31 21:23:03 +0700

    Update speedxx.js

--- speedxx.js
+++ speedxx.js
@@ -14,6 +14,15 @@ function m100() {
 
 }
 
+function mod6() {
+    h5gg.clearResults();
+    h5gg.searchNumber('909000001','I32','0x100000000', '0x160000000');
+    h5gg.editAll('909035007', 'I32');
+
+    h5gg.clearResults();
+
+
+}
 
 function m102() {
     h5gg.editAll('4397530849764387586', 'I64'); // Quay lại mã tốc độ bình thường
